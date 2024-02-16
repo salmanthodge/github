080476689ea96389ab0939d55a22dce24f056947
Salman Thodge <salmanthodge10@gmail.com>
2024-02-16 15:12:15 +0530

    third commit

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const uuid = require("uuid").v4;
 const user = require("./user");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+
+const options = {
+  origin: "*",
+  methods: "GET,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+app.use(cors(options));
 
 app.use((req, res, next) => {
   req.headers["request_id"] = uuid();
